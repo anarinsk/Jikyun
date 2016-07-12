56725a3892087167ab310904b89adf2b4dfd39eb
Junsok Huhh <anarinsk@gmail.com>
2016-07-12 20:29:31 +0900

    half ended

---
+++ freq_change/app.R
@@ -0,0 +1,58 @@
+# Data Prepare 
+
+library(dplyr)
+library(ggplot2)
+
+load("src.RData")
+
+
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- shinyUI(fluidPage(
+   
+   # Application title
+   titlePanel("Loneliness in Variety"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+       
+         sliderInput("year",
+                     "Range of years:",
+                     min = 1999,
+                     max = 2008, 
+                     value = 1999, 
+                     sep ="",
+                     tick = FALSE,
+                     animate=animationOptions(interval=250, loop=F))
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("distPlot")
+      )
+   )
+))
+
+# Define server logic required to draw a histogram
+server <- shinyServer(function(input, output) {
+   
+   output$distPlot <- renderPlot({
+      # generate bins based on input$bins from ui.R
+      my_hist(tdf, input$year)
+   })
+})
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
--- janitorjobs.R
+++ janitorjobs.R
@@ -1,10 +1,21 @@
-install.packages('readxl')
+#### Packages innstalled
+#pck_list <- c("readxl", "dplyr", "tidyr", "ggplot2")
+#install.packages(pck_list)
+#rm(pck_list)
+#### End of "Packages innstalled"
+
 library(readxl)
 library(dplyr)
 library(tidyr)
 
-# setwd("~/Documents/github/jikyun/data")
-sdf <- read_excel("seouluni_donga.xls", col_names=FALSE)
+#### Making sdf 
+
+#for MAC OS 
+# setwd("~/Documents/github/jikyun/data") 
+#for Windows 
+# setwd("~/GitHub/Jikyun")
+
+sdf <- read_excel("data/seouluni_donga.xls", col_names=FALSE)
 sdf <- tbl_df(sdf)
 sdf <- sdf %>% slice(4:n()) %>% select(2:14)
 colnames <- sdf[1,]; 
@@ -18,21 +29,55 @@ sdf <- sdf[-1,]
 names(sdf) <- colnames
 long_sdf <- sdf %>% gather(year, new_entry, Y2008:Y1999)
 
-###
+### End of "Making sdf" 
+
+### Making tables 
+### tdf: temp working df 
 
 tdf <- long_sdf
 str(long_sdf)
 
-vdf <- tdf %>% filter(new_entry>0) %>% group_by(year, HS_type) %>% summarise(sum = sum(new_entry))
-vdf <- vdf %>% spread(year, sum)
-str(vdf)
+gdf_1 <- tdf %>% filter(new_entry>0) %>% group_by(year, HS_type) %>% summarise(sum = sum(new_entry))
+gdf_2 <- function(x){tdf %>% filter(new_entry>0) %>% filter(year %in% x)} %>% arrange(new_entry)
 
 library(ggplot2)
 
-ggplot(vdf) + 
-  geom_bar(mapping = aes(x = HS_type))  
+### Font setting
 
-diamonds
+#install.packages("extrafont")
+library(extrafont)
+#font_import()
+#loadfonts()
+fonts()
 
-ggplot(vdf) +
-  geom_bar(aes(x=year, y=sum, fill=HS_type), stat ="identity", position ="fill")
+#### Bar plot with share 
+tgdf <- gdf_1 
+ggplot(tgdf) +
+  geom_bar(aes(x=year, y=sum, fill=HS_type), stat ="identity", position ="fill") + 
+  ggtitle("서울대 신입생 고교 종류별 비율") + 
+  xlab("연도") + ylab("비율") +
+  theme(
+    text = element_text(family="NanumBarunGothic"))
+
+my_hist <- function(df, my_year)
+  {
+  tyear <- paste("Y", my_year, sep="")
+  fdf <- df %>% filter(new_entry>0 & new_entry<=20) %>% filter(year %in% tyear) %>% arrange(new_entry); 
+  res <- ggplot(fdf) + 
+    theme_bw() + 
+    ggtitle("Distribution of SNU Freshmen (from 1999 to 2008)") + 
+    geom_histogram(aes(new_entry), binwidth = 0.5, fill="green") + 
+    xlim(c(0,20)) + ylim(c(0,300)) + 
+    xlab("# of SNU fresh in high school") + ylab("count") + 
+    theme(text=element_text(family="NanumBarunGothic"))
+  
+  return(res)
+  }
+
+
+my_hist(tdf, 1999)
+my_hist(tdf, 2008)
+
+### Finalize for Shiny 
+
+
